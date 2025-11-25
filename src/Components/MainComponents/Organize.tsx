9141761032fcfa7655f45c949a76c9bdffd923f5
amirsamadzadeh <amirhsmz@yahoo.com>
2025-11-25 18:03:02 +0330

    fix(organize): resolve responsive layout issue

--- src/Components/MainComponents/Organize.tsx
+++ src/Components/MainComponents/Organize.tsx
@@ -5,8 +5,8 @@ const Organize = () => {
     <div className="flex flex-col w-[90%] mx-auto lg:py-[226px] lg:flex-row lg:justify-between items-center justify-center text-center py-12.5 px-2.5 mt-20 ">
       {/* titles */}
 
-      <div className="flex flex-col items-start text-start justify-start lg:w-1/2">
-        <h2 className="font-Graphik text-5xl sm:text-6xl lg:text-5xl text-firstText w-[355px]  text-center mb-8 lg:text-start">
+      <div className="flex flex-col text-center items-center justify-center lg:items-start lg:text-start lg:justify-start lg:w-1/2 mb-15 lg:mb-0">
+        <h2 className="font-Graphik text-5xl sm:text-6xl lg:text-5xl text-firstText w-[355px]  mb-8  text-center lg:text-start">
           Fastest way to organize
         </h2>
         <span className="font-Graphik text-secondText w-64 mb-20 text-[28px] sm:text-3xl max-w-[285px] lg:hidden">
